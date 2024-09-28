3c4f382ce2080808e255dea98fea6879bf9f2f58
yasir-code <jamilyasir111@gmail.com>
2024-09-28 15:01:46 +0500

    Fixed database connection issue

--- app/api/email/route.js
+++ app/api/email/route.js
@@ -2,22 +2,25 @@ import { ConnectDB } from "@/lib/config/db";
 import EmailModel from "@/lib/models/EmailModel";
 import { NextResponse } from "next/server";
 
+// Connect to the database
 await ConnectDB();
 
-LoadDB();
+// Remove LoadDB(); since it's not defined
+
 export async function POST(request) {
   const formData = await request.formData();
   const emailData = {
     email: `${formData.get("email")}`,
   };
   await EmailModel.create(emailData);
-  return NextResponse.json({ success: true, msg: "Email Subcribed" });
+  return NextResponse.json({ success: true, msg: "Email Subscribed" });
 }
 
 export async function GET(request) {
   const emails = await EmailModel.find({});
   return NextResponse.json({ emails });
 }
+
 export async function DELETE(request) {
   const id = await request.nextUrl.searchParams.get("id");
   await EmailModel.findByIdAndDelete(id);
