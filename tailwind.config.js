2d0ffbcfd2503afbd87f778f8ad6a255fecbd0ce
yasir-code <jamilyasir111@gmail.com>
2024-09-29 21:43:02 +0500

    css issue fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./Components/**/*.{js,ts,jsx,tsx}"],
+
   theme: {
     extend: {
       backgroundImage: {
