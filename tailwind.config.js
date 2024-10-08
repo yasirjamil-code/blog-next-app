f3f45bc88d89259c52bffa1baca47757e00ccde3
yasir-code <jamilyasir111@gmail.com>
2024-10-08 14:11:01 +0500

    try to fix css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}', // Include components directory
+    './app/**/*.{js,ts,jsx,tsx}',        // app directory
+    './components/**/*.{js,ts,jsx,tsx}', // components directory
+    './lib/**/*.{js,ts,jsx,tsx}',        // lib directory (if needed)
   ],
   theme: {
     extend: {},
