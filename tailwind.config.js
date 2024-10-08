22e32c80c15cda2846a889e764209cc852c413ac
yasir-code <jamilyasir111@gmail.com>
2024-10-08 13:59:32 +0500

    css updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx}', // Add this line for app directory
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './app/**/*.{js,ts,jsx,tsx}',   // For Next.js app directory
+    './pages/**/*.{js,ts,jsx,tsx}', // For Next.js pages directory
+    './components/**/*.{js,ts,jsx,tsx}', // Include components directory
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
